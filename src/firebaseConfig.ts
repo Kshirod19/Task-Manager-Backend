559404005c41cd18c1fe79cdedad3056d65441f1
Kshirod19 <rakeshkshirod88@gmail.com>
2024-11-14 22:17:11 +0530

    Initial commit

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,18 +3,19 @@ import admin from "firebase-admin";
 import dotenv from "dotenv";
 import path from "path";
 
-// Load environment variables
+// Load environment variables from .env file
 dotenv.config();
 
-// Get the service account path dynamically
+// Get the service account path dynamically from environment variables
 const serviceAccountPath = path.resolve(__dirname, process.env.FIREBASE_PRIVATE_KEY_PATH || '');
 
-// Initialize Firebase Admin
+// Initialize Firebase Admin SDK with credentials
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccountPath),
+  credential: admin.credential.cert(serviceAccountPath), // Use the path from the .env file
   projectId: process.env.FIREBASE_PROJECT_ID,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,  // Use the environment variable for database URL
+  databaseURL: process.env.FIREBASE_DATABASE_URL, // Use the environment variable for database URL
 });
 
 const db = admin.firestore();
 export { db };
+
